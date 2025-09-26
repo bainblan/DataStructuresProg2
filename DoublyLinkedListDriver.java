b79d6dc83727815bdb84f19a52dd11054b1ef26d
Baines <brb56383@uga.edu>
2025-09-26 18:01:52 -0400

    Modified main method

--- DoublyLinkedListDriver.java
+++ DoublyLinkedListDriver.java
@@ -5,17 +5,38 @@ import java.util.Scanner;
 public class DoublyLinkedListDriver {
     
     public static void main(String[] args) {
-        DoublyLinkedList<T> list = new DoublyLinkedList<>();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter list type (i - int, d - double, s - string): ");
+        char command = input.next().charAt(0);
+        
         try (Scanner s = new Scanner(new File(args[0]))) {
-            while (s.hasNext()) {
-                list.insertItem(s.next());
+            switch (command) {
+                case 'i': {
+                    DoublyLinkedList<Integer> list = new DoublyLinkedList<>();
+                    while (s.hasNext()) {
+                        list.insertItem(Integer.parseInt(s.next()));
+                    }
+                    break;
+                }
+                case 'd': {
+                    DoublyLinkedList<Double> list = new DoublyLinkedList<>();
+                    while (s.hasNext()) {
+                        list.insertItem(Double.parseDouble(s.next()));
+                    }
+                    break;
+                }
+                case 's': {
+                    DoublyLinkedList<String> list = new DoublyLinkedList<>();
+                    while (s.hasNext()) {
+                        list.insertItem(s.next());
+                    }
+                    break;
+                }
+                default:
+                    System.out.println("Invalid type entered!");
             }
         } catch (FileNotFoundException e) {
             System.out.println("File not found!");
-            return;
         }
-        Scanner input = new Scanner(System.in);
-        char command = ' ';
-        System.out.println("Enter list type (i - int, d - double, s - string): ");
     }
 }
