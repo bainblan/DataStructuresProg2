7b924a07d02044769258e427c13f94ddb4e2e2ff
Baines <brb56383@uga.edu>
2025-10-06 18:14:04 -0400

    "added runCommands method to shorten Main method"

--- DoublyLinkedListDriver.java
+++ DoublyLinkedListDriver.java
@@ -3,6 +3,13 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+/**
+ * Driver program for the DoublyLinkedList project. Prompts the user for a list
+ * type int, double, or string, and an input file containing values of that
+ * type. The values are read from the file and inserted into a
+ * DoublyLinkedList. The user can then interactively issue commands to modify
+ * the list.
+ */
 public class DoublyLinkedListDriver {
 
     /**
@@ -21,9 +28,7 @@ public class DoublyLinkedListDriver {
         char command = input.next().charAt(0);
         // make list visible to the later command handling switch
         DoublyLinkedList list = null;
-
         try (Scanner s = new Scanner(new File(args[0]))) {
-
             switch (command) {
                 case 'i': {
                     list = new DoublyLinkedList<Integer>();
@@ -56,14 +61,12 @@ public class DoublyLinkedListDriver {
         } catch (FileNotFoundException e) {
             System.out.println("File not found!");
         }
-
         // if no list was created (invalid type or file error), exit to avoid NPEs
         if (list == null) {
             System.out.println("No list available. Exiting.");
             input.close();
             return;
         }
-
         while (command != 'q') {
             System.out.print("Enter command: ");
             command = input.next().charAt(0);
@@ -73,39 +76,52 @@ public class DoublyLinkedListDriver {
                 System.out.print("Invalid command try again: ");
                 command = input.next().charAt(0);
             }
-            switch (command) {
-                case 'q':
-                    System.out.println("Exiting the program...");
-                    break;
-                case 'i':
-                    iCalled(input, list, type);
-                    break;
-                case 'd':
-                    dCalled(input, list, type);
-                    break;
-                case 'p':
-                    pCalled(input, list, type);
-                    break;
-                case 'l':
-                    lCalled(input, list, type);
-                    break;
-                case 't':
-                    tCalled(input, list, type);
-                    break;
-                case 'r':
-                    rCalled(input, list, type);
-                    break;
-                case 'b':
-                    bCalled(input, list, type);
-                    break;
-                case 's':
-                    sCalled(input, list, type);
-                    break;
-                default:
-                    break;
-            }
+            runCommands(command, input, list, type);
         }
         input.close();
+    }
+
+    /**
+     * Execute the command specified by 'command' on the given list, using input as
+     * needed.
+     * 
+     * @param command
+     * @param input
+     * @param list
+     * @param type
+     */
+    public static void runCommands(char command, Scanner input, DoublyLinkedList list, String type) {
+        switch (command) {
+            case 'q':
+                System.out.println("Exiting the program...");
+                break;
+            case 'i':
+                iCalled(input, list, type);
+                break;
+            case 'd':
+                dCalled(input, list, type);
+                break;
+            case 'p':
+                pCalled(input, list, type);
+                break;
+            case 'l':
+                lCalled(input, list, type);
+                break;
+            case 't':
+                tCalled(input, list, type);
+                break;
+            case 'r':
+                rCalled(input, list, type);
+                break;
+            case 'b':
+                bCalled(input, list, type);
+                break;
+            case 's':
+                sCalled(input, list, type);
+                break;
+            default:
+                break;
+        }
     }
 
     /**
@@ -114,9 +130,9 @@ public class DoublyLinkedListDriver {
      * reverse.
      *
      * @param input Scanner to read user input
-     * @param list the DoublyLinkedList to operate on
-     * @param type a string indicating the value type
-     * ("number","double","string")
+     * @param list  the DoublyLinkedList to operate on
+     * @param type  a string indicating the value type
+     *              ("number","double","string")
      */
     public static void iCalled(Scanner input, DoublyLinkedList list, String type) {
         list.print();
@@ -140,9 +156,9 @@ public class DoublyLinkedListDriver {
      * the list (if present), and prints the list forwards and in reverse.
      *
      * @param input Scanner to read user input
-     * @param list the DoublyLinkedList to operate on
-     * @param type a string indicating the value type
-     * ("number","double","string")
+     * @param list  the DoublyLinkedList to operate on
+     * @param type  a string indicating the value type
+     *              ("number","double","string")
      */
     public static void dCalled(Scanner input, DoublyLinkedList list, String type) {
         list.print();
@@ -165,8 +181,8 @@ public class DoublyLinkedListDriver {
      * Handle the print command: print the list forwards.
      *
      * @param input Scanner (unused)
-     * @param list the DoublyLinkedList to print
-     * @param type the current data type (unused)
+     * @param list  the DoublyLinkedList to print
+     * @param type  the current data type (unused)
      */
     public static void pCalled(Scanner input, DoublyLinkedList list, String type) {
         list.print();
@@ -176,8 +192,8 @@ public class DoublyLinkedListDriver {
      * Handle the length command: print the number of elements in the list.
      *
      * @param input Scanner (unused)
-     * @param list the DoublyLinkedList to query
-     * @param type the current data type (unused)
+     * @param list  the DoublyLinkedList to query
+     * @param type  the current data type (unused)
      */
     public static void lCalled(Scanner input, DoublyLinkedList list, String type) {
         System.out.println("The length of the list is: " + list.length());
@@ -187,8 +203,8 @@ public class DoublyLinkedListDriver {
      * Handle the print-reverse command: print the list from tail to head.
      *
      * @param input Scanner (unused)
-     * @param list the DoublyLinkedList to print
-     * @param type the current data type (unused)
+     * @param list  the DoublyLinkedList to print
+     * @param type  the current data type (unused)
      */
     public static void tCalled(Scanner input, DoublyLinkedList list, String type) {
         list.printReverse();
@@ -198,8 +214,8 @@ public class DoublyLinkedListDriver {
      * Handle the reverse command: reverse the list in-place and print it.
      *
      * @param input Scanner (unused)
-     * @param list the DoublyLinkedList to reverse
-     * @param type the current data type (unused)
+     * @param list  the DoublyLinkedList to reverse
+     * @param type  the current data type (unused)
      */
     public static void rCalled(Scanner input, DoublyLinkedList list, String type) {
         list.reverseList();
@@ -212,8 +228,8 @@ public class DoublyLinkedListDriver {
      * the current data type.
      *
      * @param input Scanner used to read bounds
-     * @param list the DoublyLinkedList to modify
-     * @param type the current data type ("number","double","string")
+     * @param list  the DoublyLinkedList to modify
+     * @param type  the current data type ("number","double","string")
      */
     public static void bCalled(Scanner input, DoublyLinkedList list, String type) {
         try {
@@ -265,8 +281,8 @@ public class DoublyLinkedListDriver {
      * print the list and its reverse.
      *
      * @param input Scanner (unused)
-     * @param list the DoublyLinkedList to operate on
-     * @param type the current data type (unused)
+     * @param list  the DoublyLinkedList to operate on
+     * @param type  the current data type (unused)
      */
     public static void sCalled(Scanner input, DoublyLinkedList list, String type) {
         try {
