7522428d95dbade7bc2c4f1a3085ac01487cb3c6
CC1358952 <clc51854@uga.edu>
2025-10-06 13:48:11 -0400

    Update DoublyLinkedListDriver.java
    Second shift created, filler print statements

--- DoublyLinkedListDriver.java
+++ DoublyLinkedListDriver.java
@@ -1,19 +1,22 @@
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class DoublyLinkedListDriver {
-    
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter list type (i - int, d - double, s - string): ");
         char command = input.next().charAt(0);
-        
+        // make list visible to the later command handling switch
+        DoublyLinkedList list = null;
+
         try (Scanner s = new Scanner(new File(args[0]))) {
 
             switch (command) {
                 case 'i': {
-                    DoublyLinkedList<Integer> list = new DoublyLinkedList<>();
+                    list = new DoublyLinkedList<Integer>();
                     while (s.hasNext()) {
                         list.insertItem(Integer.parseInt(s.next()));
                         System.out.println("");
@@ -21,14 +24,14 @@ public class DoublyLinkedListDriver {
                     break;
                 }
                 case 'd': {
-                    DoublyLinkedList<Double> list = new DoublyLinkedList<>();
+                    list = new DoublyLinkedList<Double>();
                     while (s.hasNext()) {
                         list.insertItem(Double.parseDouble(s.next()));
                     }
                     break;
                 }
                 case 's': {
-                    DoublyLinkedList<String> list = new DoublyLinkedList<>();
+                    list = new DoublyLinkedList<String>();
                     while (s.hasNext()) {
                         list.insertItem(s.next());
                     }
@@ -42,6 +45,13 @@ public class DoublyLinkedListDriver {
             System.out.println("File not found!");
         }
 
+        // if no list was created (invalid type or file error), exit to avoid NPEs
+        if (list == null) {
+            System.out.println("No list available. Exiting.");
+            input.close();
+            return;
+        }
+
         while (command != 'q') {
             System.out.print("Enter command: ");
             command = input.next().charAt(0);
@@ -51,51 +61,63 @@ public class DoublyLinkedListDriver {
                 System.out.print("Invalid command try again: ");
                 command = input.next().charAt(0);
             }
-            if (command == 'q') {
-                System.out.println("Exiting the program...");
-            } else if (command == 'i') {
-                iCalled(input, list);
-            } else if (command == 'd') {
-                dCalled(input, list);
-            } else if (command == 's') {
-                sCalled(input, list);
-            } else if (command == 'a') {
-                aCalled(input, list);
-            } else if (command == 'm') {
-                mCalled(input, list);
-            } else if (command == 't') {
-                tCalled(input, list);
-            } else if (command == 'p') {
-                System.out.print("The list is: ");
-                list.printList();
-            } else if (command == 'l') {
-                System.out.println("The length of the list is " + list.getLength());
+            switch (command) {
+                case 'q':
+                    System.out.println("Exiting the program...");
+                    break;
+                case 'i':
+                    iCalled(input, list);
+                    break;
+                case 'd':
+                    dCalled(input, list);
+                    break;
+                case 's':
+                    sCalled(input, list);
+                    break;
+                case 'a':
+                    aCalled(input, list);
+                    break;
+                case 'm':
+                    mCalled(input, list);
+                    break;
+                case 't':
+                    tCalled(input, list);
+                    break;
+                case 'p':
+                    System.out.print("The list is: ");
+                    list.print();
+                    break;
+                case 'l':
+                    System.out.println("The length of the list is " + list.length());
+                    break;
+                default:
+                    break;
             }
         }
         input.close();
     }
 
     public static void dCalled(Scanner input, DoublyLinkedList list) {
-
+        System.out.println("dCalled executed");
     }
 
     public static void iCalled(Scanner input, DoublyLinkedList list) {
-
+        System.out.println("iCalled executed");
     }
 
-    public static void sCalled(Scanner input, DoublyLinkedList list) { 
-
+    public static void sCalled(Scanner input, DoublyLinkedList list) {
+        System.out.println("sCalled executed");
     }
 
-    public static void mCalled(Scanner input, DoublyLinkedList list) { 
-
+    public static void mCalled(Scanner input, DoublyLinkedList list) {
+        System.out.println("mCalled executed");
     }
 
-    public static void aCalled(Scanner input, DoublyLinkedList list) { 
-    
+    public static void aCalled(Scanner input, DoublyLinkedList list) {
+        System.out.println("aCalled executed");
     }
 
-    public static void tCalled(Scanner input, DoublyLinkedList list) { 
-
+    public static void tCalled(Scanner input, DoublyLinkedList list) {
+        System.out.println("tCalled executed");
     }
 }
